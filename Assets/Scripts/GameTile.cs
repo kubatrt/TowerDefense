42890e064a10ee0134d47627827b5e58b3c64b0c
Jacob <kubatrt@gmail.com>
2019-10-10 10:48:32 +0200

    Tile neighbors.

--- Assets/Scripts/GameTile.cs
+++ Assets/Scripts/GameTile.cs
@@ -6,4 +6,21 @@ public class GameTile : MonoBehaviour
 {
     [SerializeField]
     Transform arrow = default;
+
+    GameTile north, east, west, south, nextOnPath;
+    int distance;
+
+    public static void MakeEastWestNeighbors(GameTile east, GameTile west)
+    {
+        Debug.Assert(west.east == null && east.west == null, "Redefines neighbors!");
+        west.east = east;
+        east.west = west;
+    }
+
+    public static void MakeNorthSouthNeighbors(GameTile north, GameTile south)
+    {
+        Debug.Assert(south.north == null && north.south == null, "Redefines neighbors!");
+        south.north = north;
+        north.south = south;
+    }
 }
